edd30d7d94bd7c26f530bf7f6a053c5943d12b1c
vsamartzis <vsamartzis@trading-point.com>
2026-02-13 17:23:18 +0200

    Import godotenv module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vsamartzis/awesomeProject
 
 go 1.25.7
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Printf("Hello and asdadswelcome!")
+	portString := os.Getenv("PORT")
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if portString == "" {
+		log.Fatal("PORT is not found on the environment")
+	}
+
+	fmt.Println("Port: :", portString)
 }
